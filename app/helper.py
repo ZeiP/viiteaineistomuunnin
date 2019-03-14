b8b41208595f5c89a422cd226bed518b32642914
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2019-03-14 23:35:42 +0200

    Remove spaces from reference number fields

--- app/helper.py
+++ app/helper.py
@@ -12,14 +12,14 @@ def transform_op(data, account, transfer):
       amount = float(row[2].replace(',', '.'))
     except ValueError:
       continue
-    if amount > 0 and (transfer == 'all' or unicode(row[7]).isnumeric()):
+    if amount > 0 and (transfer == 'all' or row[7].replace(' ', '').isnumeric()):
       line = ReferenceTransferLine()
       line.account_no = format_account(account)
       line.booking_date = datetime.strptime(row[1], '%d.%m.%Y')
       line.payment_date = datetime.strptime(row[0], '%d.%m.%Y')
       line.archive_id = row[9]
-      if row[7].isnumeric():
-        line.reference_no = row[7]
+      if row[7].replace(' ', '').isnumeric():
+        line.reference_no = row[7].replace(' ', '')
       else:
         line.reference_no = '0'
       line.payer = row[5]
@@ -35,14 +35,14 @@ def transform_nordea(data, account, transfer):
       amount = float(row[3].replace(',', '.'))
     except ValueError:
       continue
-    if amount > 0 and (transfer == 'all' or unicode(row[8]).isnumeric()):
+    if amount > 0 and (transfer == 'all' or row[8].replace(' ', '').isnumeric()):
       line = ReferenceTransferLine()
       line.account_no = format_account(account)
       line.booking_date = datetime.strptime(row[1], '%d.%m.%Y')
       line.payment_date = datetime.strptime(row[0], '%d.%m.%Y')
       line.archive_id = ' '
-      if unicode(row[8]).isnumeric():
-        line.reference_no = row[8]
+      if row[8].replace(' ', '').isnumeric():
+        line.reference_no = row[8].replace(' ', '')
       else:
         line.reference_no = '0'
       line.payer = row[4]
@@ -58,14 +58,14 @@ def transform_kuksa(data, account, transfer):
       amount = float(row[8].replace(',', '.'))
     except ValueError:
       continue
-    if amount > 0 and (transfer == 'all' or unicode(row[4]).isnumeric()):
+    if amount > 0 and (transfer == 'all' or row[4].replace(' ', '').isnumeric()):
       line = ReferenceTransferLine()
       line.account_no = format_account(account)
       line.booking_date = date.today()
       line.payment_date = date.today()
       line.archive_id = 'VIITEMUUNNIN'
-      if unicode(row[4]).isnumeric():
-        line.reference_no = row[4]
+      if row[4].replace(' ', '').isnumeric():
+        line.reference_no = row[4].replace(' ', '')
       else:
         line.reference_no = '0'
       line.payer = row[3]
